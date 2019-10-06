11878a02655cbd58de18e5cc07472995c7664c08
Sava <vukosavljevs@gmail.com>
2019-10-06 19:27:06 +0200

    first commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentsContainer from './components/StudentsContainer'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentsContainer />
+    </div>
+  );
+}
+
+export default App;
